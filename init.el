e080935b7cc2156b38419e2fc8b815b0107ef371
Priyanshu Jain <priyanshu@launchyard.com>
2018-02-13 08:48:00 +0530

    Add go config

--- init.el
+++ init.el
@@ -1,1 +1,62 @@
+
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+(require 'init-benchmarking) ;; Measure startup time
+
+;; melpha config
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+		    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
+(package-initialize)
+
+;; GO config start
+
+(require 'go-mode)
+
+;; path for godoc
+(defun set-exec-path-from-shell-PATH ()
+  (let ((path-from-shell (replace-regexp-in-string
+			  "[ \t\n]*$"
+			  ""
+			  (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
+    (setenv "PATH" path-from-shell)
+    (setq eshell-path-env path-from-shell) ; for eshell users
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+(when window-system (set-exec-path-from-shell-PATH))
+
+;; Go path already set in dotfiles
+;; (setenv "GOPATH" "~/Workspace/gocode")
+
+;; autoformat on save using gofmt
+(add-to-list 'exec-path "/Users/tleyden/Development/gocode/bin")
+(add-hook 'before-save-hook 'gofmt-before-save)
+
+;; auto-complete
+(defun auto-complete-for-go ()
+  (auto-complete-mode 1))
+(add-hook 'go-mode-hook 'auto-complete-for-go)
+
+
+
+;; go config end
+
+
+;; Load theme
+
 (load-theme 'tsdh-dark)
+
+;;  auto pair braces
+(electric-pair-mode 1)
+
+
+;; slime config
+;; Set your lisp system and, optionally, some contribs
+(setq inferior-lisp-program "/usr/bin/sbcl")
+(setq slime-contribs '(slime-fancy))
